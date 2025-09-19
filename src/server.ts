bfc04c8104ed5fc5e1a814800fa4d95c8a707085
AABB2741 <mariosantos88477364484@gmail.com>
2025-09-19 00:37:33 -0300

    feat: create restaurant

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,41 @@
 import Elysia from "elysia";
+import { z } from "zod";
 
-const app = new Elysia();
+import { restaurants, users } from "./db/schema/index.ts";
+import { db } from "./lib/drizzle.ts";
 
-app.get("/", "Ok");
+const app = new Elysia().get("/", "Ok").post(
+  "/restaurants",
+  async ({ status, body }) => {
+    const { restaurantName, managerName, email, phone } = body;
+
+    const [manager] = await db
+      .insert(users)
+      .values({
+        name: managerName,
+        email,
+        phone,
+        role: "manager",
+      })
+      .returning();
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager?.id,
+    });
+
+    return status(204);
+  },
+  {
+    body: z.object({
+      restaurantName: z.string(),
+      managerName: z.string(),
+      phone: z.string(),
+      email: z.email(),
+    }),
+    response: {},
+  },
+);
 
 app.listen(3333, () => {
   console.log("Server running at http://localhost:3333");
